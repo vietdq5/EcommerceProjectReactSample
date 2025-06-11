e6780a0253590e4a32747606c96549058bdd42cb
qvietd <qvietd@gmail.com>
2025-06-11 17:36:17 +0700

    add infinite scroll

--- src/components/Product/ProductsList.jsx
+++ src/components/Product/ProductsList.jsx
@@ -4,28 +4,47 @@ import useDataHook from '../../hooks/useDataHook';
 import ProductLoadingSkeleton from './ProductLoadingSkeleton';
 import { useSearchParams } from 'react-router-dom';
 import Pagination from '../Common/Pagination';
+import { useState, useEffect } from 'react';
 
 const ProductsList = () => {
-    // const [page, setPage] = useState(1);
+    const [page, setPage] = useState(1);
     const [search, setSearch] = useSearchParams();
     const category = search.get("category");
-    const page = search.get("page");
+    // const page = search.get("page");
     const { data, error, isLoading } = useDataHook("/products", {
         params: {
             category,
-            page
+            page,
+            perPage: 10
         }
     }, [category, page]);
 
-    // useEffect(() => {
-    //     setPage(1);
-    // }, [category]);
+    useEffect(() => {
+        setPage(1);
+    }, [category]);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const { scrollTop, clientHeight, scrollHeight } =
+                document.documentElement;
+            if (
+                scrollTop + clientHeight >= scrollHeight - 1 &&
+                !isLoading &&
+                data &&
+                page < data.totalPages
+            ) {
+                setPage((prev) => prev + 1);
+            }
+        };
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll)
+    }, [data, isLoading]);
 
     const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    const handlePageChange = (page) => {
-        const currentParams = Object.fromEntries([...search]);
-        setSearch({ ...currentParams, page: page })
-    };
+    // const handlePageChange = (page) => {
+    //     const currentParams = Object.fromEntries([...search]);
+    //     setSearch({ ...currentParams, page: page })
+    // };
 
     return (
         <section className="products_list_section">
@@ -43,15 +62,15 @@ const ProductsList = () => {
             <div className='products_list'>
                 {error && <em className='form_error'>{error}</em>}
                 {
-                    isLoading ?
-                        skeletons.map((index) => <ProductLoadingSkeleton key={index} />)
-                        :
-                        data?.products && data?.products.map((product) => (
-                            <ProductCard key={product._id} product={product} />
-                        ))
+                    isLoading && skeletons.map((index) => <ProductLoadingSkeleton key={index} />)
+                }
+                {
+                    data?.products && data?.products.map((product) => (
+                        <ProductCard key={product._id} product={product} />
+                    ))
                 }
             </div>
-            <Pagination totalItems={data?.totalProducts || 0} itemPerPage={data?.postPerPage || 0} handleClick={handlePageChange} currentPage={page} />
+            {/* <Pagination totalItems={data?.totalProducts || 0} itemPerPage={data?.postPerPage || 0} handleClick={handlePageChange} currentPage={page} /> */}
         </section>
     );
 }
