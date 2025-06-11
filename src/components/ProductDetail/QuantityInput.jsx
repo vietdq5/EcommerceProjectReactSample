0721f96215aba2e0bdba72d9b21f4dcec9421913
qvietd <qvietd@gmail.com>
2025-06-11 19:16:20 +0700

    update usememo to optimazi page rendering

--- src/components/ProductDetail/QuantityInput.jsx
+++ src/components/ProductDetail/QuantityInput.jsx
@@ -2,23 +2,30 @@
 import './QuantityInput.css'
 import { useState, memo } from 'react';
 
-const QuantityInput = ({ quantity, setQuantity, stock }) => {
+const QuantityInput = memo(({ stock }) => {
+    const [quantity, setQuantity] = useState(1);
+
+    const handleQuantityChange = (value) => {
+        if (value >= 1 && value <= stock) {
+            setQuantity(value);
+        }
+    };
     return (
         <>
             <button
                 className='quantity_input_button'
-                onClick={() => setQuantity(quantity - 1)}
+                onClick={() => handleQuantityChange(quantity - 1)}
                 disabled={quantity <= 1}>
                 -
             </button>
             <p className='quantity_input_count'>{quantity}</p>
             <button
                 className='quantity_input_button'
-                onClick={() => setQuantity(quantity + 1)}
+                onClick={() => handleQuantityChange(quantity + 1)}
                 disabled={quantity >= stock}>
                 +
             </button>
         </>
     )
-}
+});
 export default QuantityInput;
